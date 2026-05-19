63dc9df7280e2d7dea8088ab1f9119f35a223795
nerissa23 <u2101552@siswa.um.edu.my>
2026-05-20 00:02:12 +0800

    refactor: enhance prompt_model for better readability and modularity

--- week_2/prompt_model.py
+++ week_2/prompt_model.py
@@ -9,39 +9,56 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv("GOOGLE_API_KEY")
 
-def prompt_model(model: str, prompt: str) -> str :
-    """Use model selected to generate content from text prompt;
-    Returns text response"""
-
-    # check if prompt is valid: truthy strings
-    if not prompt:
-        return f"Please enter a proper text prompt."
-    
-    ollama_models = ['deepseek-r1:1.5b', 'phi3', 'llama3.1']
+def ollama_prompt(model: str, prompt: str) -> str:
+    """Use `ollama.generate()` to run one-shot prompts with ollama model"""
 
     try:
-        # if model --> ollama models:
-        if any(elem in model for elem in ollama_models):
-            res = generate(
-                model=model,
-                prompt=prompt
-            )
-            print("\n--- RESPONSE ---\n")
-            return res.response
+        res = generate(
+            model=model,
+            prompt=prompt
+        )
+        return res.response
+    except Exception as e:
+        return f"[Ollama Error] {e}"
 
-        # if model --> google/gemini models:
+def gemini_prompt(model: str, prompt: str) -> str:
+    """Run gemini model specified to generate content"""
+
+    try:
         client = genai.Client(api_key=API_KEY)
         res = client.models.generate_content(
             model=model,
             contents=prompt
         )
-        print("\n--- RESPONSE ---\n")
         return res.text
     except errors.APIError as e:
-        # catch errors returned by google's api server
-        print(f"[Gemini Error] {e.code} {e.status}. {e}")
+        return f"[Gemini Error] {e.code} {e.status}. {e}"
     except Exception as e:
-        print(f"Function error: {e}")
+        return f"{type(e).__name__}: {e}"
+
+def prompt_model(model: str, prompt: str) -> str :
+    """Use model selected to generate content from text prompt;
+    Returns text response"""
+
+    # check if prompt is valid: truthy strings
+    if not prompt.strip():
+        return "Please enter a proper text prompt."
+    
+    ollama_models = ['deepseek-r1:1.5b', 'phi3', 'llama3.1']
+    google_models = ['gemini-2.5-flash', 'gemini-2.5-flash-lite', 'gemini-3-flash-preview']
+
+    # if model --> ollama models:
+    if any(elem in model for elem in ollama_models):
+        res = ollama_prompt(model, prompt)
+
+    # if model --> google/gemini models:
+    elif any(elem in model for elem in google_models):
+        res = gemini_prompt(model, prompt)
+
+    else:
+        return f"Model selected is not available. Please select any of the following models: \n--> ollama models -> {ollama_models} \n--> google models -> {google_models}"
+    
+    return res
 
 def main():
     # strictly enforce only 2 args to pass into prompt_model()
@@ -49,6 +66,7 @@ def main():
         model_name = sys.argv[1]
         text_prompt = sys.argv[2]
         response = prompt_model(model_name, text_prompt)
+        print("\n--- RESPONSE ---\n")
         if response: print(response)
     else:
         print("Usage: uv run prompt_model.py [model-name] [text-prompt]")
